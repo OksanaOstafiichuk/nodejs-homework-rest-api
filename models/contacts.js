67aa9a6712e2adc3eb11ea58d599629d3255fadf
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-09-21 12:25:07 +0300

    gets all contacts and contact by id

--- models/contacts.js
+++ models/contacts.js
@@ -1,8 +1,23 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json')
+// console.log(contactsPath)
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const date = await fs.readFile(contactsPath);
+  const getContacts = JSON.parse(date);
+
+  return getContacts;
+}
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contactById = contacts.find(contact => String(contact.id) === String(contactId));
+
+
+  return contactById;
+}
 
 const removeContact = async (contactId) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const { listContacts, getContactById } = require('../../models/contacts.js')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts)
+    
+  } catch (error) {
+
+    res.status(500).json({message: error.messages})
+  }
+
+  next()
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contactId = req.params.contactId;
+    const contactById = await getContactById(contactId);
+
+    if (!contactById) {
+          res.status(404).json({message: "Not found"})
+    }
+
+    res.status(200).json(contactById)
+  } catch (error) {
+    
+    res.status(404).json({message: "Not found"})
+  }
+
+  next()
 })
 
 router.post('/', async (req, res, next) => {
