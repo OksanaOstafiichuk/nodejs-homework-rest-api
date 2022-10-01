f9b342a2a031a7325d7cea796908ebda37c3d91f
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-10-01 23:49:20 +0300

    adds middleware for validation by id

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,16 @@
 const express = require("express");
 
 const { getAll, getById, add, removeById, updateById, updateByFavorite } = require('../../controllers/contacts');
-const { validateBody } = require('../../middlewares');
+const { validateBody, isValidId } = require('../../middlewares');
 const { bodySchema, updateByFavoriteSchema } = require('../../schemas/contacts');
 
 const router = express.Router();
 
 router.get("/", getAll);
-router.get("/:contactId", getById);
+router.get("/:contactId", isValidId, getById);
 router.post("/", validateBody(bodySchema),add);
-router.delete("/:contactId", removeById);
-router.put("/:contactId", validateBody(bodySchema), updateById);
-router.patch("/:contactId/favorite", validateBody(updateByFavoriteSchema),updateByFavorite);
+router.delete("/:contactId", isValidId, removeById);
+router.put("/:contactId", isValidId, validateBody(bodySchema), updateById);
+router.patch("/:contactId/favorite", isValidId, validateBody(updateByFavoriteSchema),updateByFavorite);
 
 module.exports = router;
