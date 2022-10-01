bfc02ee7752827c58543cb675257b402778c20ab
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-10-01 21:54:51 +0300

    adds validateBody middleware

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
-const {getAll, getById, add, removeById, updateById} = require('../../controllers/contacts')
+const { getAll, getById, add, removeById, updateById } = require('../../controllers/contacts');
+const { validateBody } = require('../../middlewares');
+const { bodySchema } = require('../../schemas');
 
 const router = express.Router();
 
 router.get("/", getAll);
 router.get("/:contactId", getById);
-router.post("/", add);
+router.post("/", validateBody(bodySchema),add);
 router.delete("/:contactId", removeById);
 router.put("/:contactId", updateById);
 
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,14 +1,15 @@
 const Joi = require("joi");
 
 const bodySchema = Joi.object({
-  name: Joi.string().alphanum().min(3).required(),
+  name: Joi.string().required(),
 
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
     .required(),
 
   phone: Joi.string()
     .required(),
+  
+  favorite: Joi.boolean()
 });
 
 module.exports = bodySchema;
