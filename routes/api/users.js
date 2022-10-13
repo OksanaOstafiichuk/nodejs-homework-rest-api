44d95463a0d8ea84c4ed9b12ad784a0634db0da7
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-10-13 20:47:15 +0300

    receives user data by token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { register, login, logout } = require('../../controllers/users');
+const { register, login, logout, currentUser } = require('../../controllers/users');
 const { validateBody, authenticate } = require('../../middlewares');
 const {registerSchema, loginSchema} = require('../../schemas/users')
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/register', validateBody(registerSchema), register);
 router.post('/login', validateBody(loginSchema), login);
 router.get('/logout', authenticate, logout);
+router.get('/current', authenticate, currentUser);
 
 module.exports = router;
