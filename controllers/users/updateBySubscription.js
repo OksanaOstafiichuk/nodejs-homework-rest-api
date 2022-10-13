91cf0af8b7474a7b86fd5f1b09b056b89587ea14
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-10-13 22:23:41 +0300

    Update updateBySubscription.js

--- controllers/users/updateBySubscription.js
+++ controllers/users/updateBySubscription.js
@@ -1,11 +1,16 @@
 const User = require('../../models/user');
+const { RequestError } = require('../../helpers');
 
 const updateBySubscription = async (req, res, next) => {
     try {
         const { subscription } = req.body;
         const { _id } = req.user;
-        const user = await User.findByIdAndUpdate(_id, { subscription });
-
+        const user = await User.findByIdAndUpdate(_id, { subscription }, {new: true});
+        
+        if (!user) {
+            throw RequestError(401, "Not authorized");
+        }
+        
         res.json({ subscription: user.subscription })        
     } catch (error) {
         next(error)
