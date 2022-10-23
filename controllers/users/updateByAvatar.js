157339593acba5d3706657f8806fccb2b6762a6e
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-10-23 18:45:02 +0300

    changes resize avatar image

--- controllers/users/updateByAvatar.js
+++ controllers/users/updateByAvatar.js
@@ -1,3 +1,4 @@
+const Jimp = require('jimp');
 const fs = require('fs/promises');
 const path = require('path');
 
@@ -12,6 +13,10 @@ const updateByAvatar = async (req, res, next) => {
         const filename = `${_id}.${extension}`;
         const uploadDir = path.join(__dirname, '../../', 'public', 'avatars');
         const uploadPath = path.join(uploadDir, filename);
+
+        const image = await Jimp.read(tmpDir);
+        await image.resize(250, 250)
+             .writeAsync(tmpDir);
 
         await fs.rename(tmpDir, uploadPath);
         const avatarURL = path.join('avatars', filename);
