657e57a068f887ec4c62e72a78c2f8d54c7822cc
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-10-31 12:36:50 +0200

    creates sendMail helper

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const RequestError = require('./RequestError');
 const handleSaveErrors = require('../helpers/handleSaveErrors');
+const sendMail = require('./sendMail');
 
 module.exports = {
     RequestError,
     handleSaveErrors,
+    sendMail,
 }
